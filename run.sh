48fddafe40660cc3bd1a5c2a7ff118b5d4df1d5f
mishrasunny174 <mishrasunny174@gmail.com>
2021-04-02 22:38:54 +0530

    updated cpu count to 4

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ qemu-system-aarch64 -m 2G -M virt -cpu max \
   -bios ./QEMU_EFI.fd \
   -drive if=none,file=debian-10-arm64.qcow2,id=hd0 -device virtio-blk-device,drive=hd0 \
   -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp:127.0.0.1:2222-:22 \
+  -smp cpus=4 \
   -nographic
